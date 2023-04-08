c908d6da53add0ff6b998b97a9508108c190c6e8
test <test@test.com>
2023-04-08 16:39:01 +0900

    feat: 날짜 기록 및, 최신화 적용

--- dataProcessing.py
+++ dataProcessing.py
@@ -7,13 +7,20 @@ import json
 import xmltodict
 from dotenv import load_dotenv
 import requests
+import pandas as pd
+from datetime import datetime, timedelta
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 group_chat_id = os.getenv("GROUP_CHAT_ID")
 apikey = os.getenv("API_KEY")
 
-date = 20230111
+
+def dateDatetimeToInt(datatime):
+    return int(datatime.strftime("%Y%m%d"))
+
+def dateIntToDatetime(dateInt):
+    return datetime.strptime(str(dateInt),'%Y%m%d')
 
 def tel_send_message_to_group(chat_id, text):
     url = f'https://api.telegram.org/bot{TOKEN}/sendmessage?chat_id={chat_id}&text={text}'   
@@ -21,10 +28,15 @@ def tel_send_message_to_group(chat_id, text):
     return r
 
 def load_opendart_data(apikey, date):
-    os.chdir("./files/")
+    print("load date: ", date)
+    
     res1 = requests.get("https://opendart.fss.or.kr/api/list.json?crtfc_key={}&bgn_de={}&end_de={}&pblntf_ty=D&page_no=1&page_count=100".format(apikey, date, date))
     doc_num_lst = []
     doc_lst = json.loads(res1.content)
+    
+    if doc_lst['message'] != "정상" :
+      print(doc_lst['message'])
+      return
     
     for dc in doc_lst['list']:
       doc_num_lst.append(dc['rcept_no'])
@@ -45,9 +57,9 @@ def load_opendart_data(apikey, date):
                 xmlString = f.read()
             check1 = xmlString.find("장내매수")
             check2 = xmlString.find("장내 매수")
-
-            if check1 != -1 or check2 != -1 :
-                print("장내 매수 발생")
+            check3 = xmlString.find("대량보유상황보고서")
+            if (check1 != -1 or check2 != -1) and check3 == -1 :
+                print("장내 매수")
                 try:
                     jsonString = json.dumps(xmltodict.parse(xmlString), indent=4, ensure_ascii = False)
                     document = json.loads(jsonString)
@@ -59,6 +71,7 @@ def load_opendart_data(apikey, date):
                     companyname = document['DOCUMENT']['COMPANY-NAME']['#text']
                     doc_title =  document['DOCUMENT']['DOCUMENT-NAME']['#text']
                     tele_msg = "장내 매수 발생\n회사명 : {}\n일시 : {}\n문서 제목 : {}\n링크 : {}\n".format(companyname, date, doc_title, link)
+                    print(tele_msg)
                     tel_send_message_to_group(group_chat_id, tele_msg)
                 except:
                     print("json load 실패, 문서 정보 불러오기 실패")
@@ -68,7 +81,8 @@ def load_opendart_data(apikey, date):
                     link = "https://dart.fss.or.kr/dsaf001/main.do?rcpNo={}".format(doc_id)
                     companyname = "불러오기 실패"
                     doc_title = "불러오기 실패"
-                    tele_msg = "장내 매수 발생\n회사명 : {}\n일시 : {}\n문서 제목 : {}\n링크 : {}\n".format(companyname, date, doc_title, link)
+                    tele_msg = "장내 매수 \n회사명 : {}\n일시 : {}\n문서 제목 : {}\n링크 : {}\n".format(companyname, date, doc_title, link)
+                    print(tele_msg)
                     tel_send_message_to_group(group_chat_id, tele_msg)
             xmlString = ""
         except:
@@ -78,6 +92,46 @@ def load_opendart_data(apikey, date):
 
         if doc_name in file_list:
             os.remove(doc_name)
+    print()
+
+def main():
+    current_working_directory = os.getcwd()
+    detail_file_path = "/files/"
+    os.chdir(current_working_directory+detail_file_path)
+    past_date = pd.read_csv('dateLog.csv')
+
+    past_date_lst = list(past_date["data_log"])
+    last_date = past_date_lst[-1]
+    today = int(datetime.today().strftime("%Y%m%d"))
+    print("today: {}, latest date: {}".format(today, last_date))
+
+    diff = dateIntToDatetime(today) - dateIntToDatetime(last_date)
+    if (diff.days == 1):
+       print("모든 데이터가 최신화 되어 있습니다. 프로그램을 종료합니다.")
+       exit()
+    print("diff :", diff)
+    new_date_lst = []
+
+    for i in range(diff.days):
+      if i == 0:
+        next_date = dateIntToDatetime(last_date)
+        continue
+      next_date = next_date + timedelta(days=1)
+      new_date_lst.append(dateDatetimeToInt(next_date))
 
 
-load_opendart_data(apikey, date)
+    for call_date in new_date_lst:
+        load_opendart_data(apikey, call_date)
+
+    print("alert success date :", new_date_lst)
+
+    for date in new_date_lst:
+      past_date_lst.append(date)
+
+    date_df =  pd.DataFrame(past_date_lst, columns=['data_log'])
+    date_df.to_csv("dateLog.csv", index = False)
+
+    
+
+if __name__ == "__main__":
+   main()
